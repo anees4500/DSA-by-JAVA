30f80500b4e22d3760d9df79c2e773d2c264d1dd
anees4500 <mohammadanees4500@gmail.com>
2025-10-17 16:09:25 +0530

    Time: 1 ms (99.71%) | Memory: 41.9 MB (90.01%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,28 +1,42 @@
+ import java.util.*;
+
 class Solution {
+    Map<String, List<Integer>> memo = new HashMap<>();
+
     public List<Integer> diffWaysToCompute(String expression) {
         
+        if (memo.containsKey(expression)) {
+            return memo.get(expression);
+        }
+
         List<Integer> res = new ArrayList<>();
 
-        for(int i = 0; i<expression.length(); i++){
+        for (int i = 0; i < expression.length(); i++) {
             char c = expression.charAt(i);
-            if(c=='+' || c=='*' || c=='-' ){
+
+            if (c == '+' || c == '-' || c == '*') {
+                
                 List<Integer> left = diffWaysToCompute(expression.substring(0, i));
-                List<Integer> right = diffWaysToCompute(expression.substring(i+1));
+                List<Integer> right = diffWaysToCompute(expression.substring(i + 1));
 
-
-                for(int a : left){
-                    for(int b : right){
-                        if(c=='+') res.add(a+b);
-                        else if(c=='-') res.add(a-b);
-                        else if(c=='*') res.add(a*b);
+                // Combine results
+                for (int a : left) {
+                    for (int b : right) {
+                        if (c == '+') res.add(a + b);
+                        else if (c == '-') res.add(a - b);
+                        else if (c == '*') res.add(a * b);
                     }
                 }
-            }  
+            }
         }
 
-        if(res.isEmpty()){
+        // Base case: expression is a single number
+        if (res.isEmpty()) {
             res.add(Integer.parseInt(expression));
         }
+
+        // Store in memo before returning
+        memo.put(expression, res);
         return res;
     }
 }
