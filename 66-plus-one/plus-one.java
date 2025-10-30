8c7416be82ce4a3c59ce0cd39113edae3eb35631
anees4500 <mohammadanees4500@gmail.com>
2025-10-30 20:40:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (9.80%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,23 +1,16 @@
  class Solution {
     public int[] plusOne(int[] digits) {
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < digits.length; i++) {
-            sb.append(digits[i]);
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits; // done, no carry
+            }
+            digits[i] = 0; // carry over
         }
 
-        // Use BigInteger to handle large numbers safely
-        java.math.BigInteger num = new java.math.BigInteger(sb.toString());
-        num = num.add(java.math.BigInteger.ONE);
-
-        String str = num.toString();
-
-        int[] dig = new int[str.length()];
-        for (int i = 0; i < str.length(); i++) {
-            dig[i] = str.charAt(i) - '0'; // ✅ convert char to int
-        }
-
-        return dig;
+        // If all digits were 9 → new array needed
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
     }
 }
