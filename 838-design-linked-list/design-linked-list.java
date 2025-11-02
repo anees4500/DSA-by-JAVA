56b14db147121fb8298c1c816f49c58bfca2bd24
anees4500 <mohammadanees4500@gmail.com>
2025-11-02 12:13:20 +0530

    Time: 10 ms (11.85%) | Memory: 47 MB (5.30%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.java
@@ -0,0 +1,89 @@
+ class MyLinkedList {
+    // Node definition
+    private class Node {
+        int val;
+        Node next;
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    // Get the value of the index-th node
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node curr = head;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        return curr.val;
+    }
+
+    // Add a node at the head
+    public void addAtHead(int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    // Add a node at the tail
+    public void addAtTail(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+        }
+        size++;
+    }
+
+    // Add a node before the index-th node
+    public void addAtIndex(int index, int val) {
+        if (index > size) return;
+        if (index <= 0) {
+            addAtHead(val);
+            return;
+        }
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
+
+        Node newNode = new Node(val);
+        Node curr = head;
+        for (int i = 0; i < index - 1; i++) {
+            curr = curr.next;
+        }
+        newNode.next = curr.next;
+        curr.next = newNode;
+        size++;
+    }
+
+    // Delete the index-th node
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node curr = head;
+            for (int i = 0; i < index - 1; i++) {
+                curr = curr.next;
+            }
+            curr.next = curr.next.next;
+        }
+        size--;
+    }
+}
