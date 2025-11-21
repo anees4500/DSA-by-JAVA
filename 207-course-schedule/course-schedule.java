031f07fb6ab65f787ff7693dacb33db41926c3eb
anees4500 <mohammadanees4500@gmail.com>
2025-11-21 19:17:37 +0530

    Time: 4 ms (91.40%) | Memory: 46.9 MB (28.74%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,45 +1,43 @@
- import java.util.*;
-
 class Solution {
 
-    public boolean dfs(int node, List<List<Integer>> adj, int[] vis, int[] pathVis) {
-        vis[node] = 1;       // mark visited
-        pathVis[node] = 1;   // mark current recursion path
-
-        for (int it : adj.get(node)) {
-            if (vis[it] == 0) {
-                if (dfs(it, adj, vis, pathVis)) return true;  // cycle found
-            } else if (pathVis[it] == 1) {
-                return true;  // cycle found
+    public boolean dfs(int src , List<List<Integer>> adj, boolean vis[], boolean stack[]){
+        vis[src] = true;
+        stack[src] = true;
+        for(int i : adj.get(src)){
+            if(!vis[i]){
+                if(dfs(i,adj,vis,stack)) return true;
+            }
+            else if(stack[i]){
+                return true;    // cycle found
             }
         }
-
-        pathVis[node] = 0;  // backtrack
-        return false;
+        stack[src] = false;
+        return false; 
     }
-
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+ 
+    
+    public boolean canFinish(int numC, int[][] pre) {
         List<List<Integer>> adj = new ArrayList<>();
 
-        // Build adjacency list
-        for (int i = 0; i < numCourses; i++) {
+        for(int i = 0; i<numC; i++){
             adj.add(new ArrayList<>());
         }
 
-        for (int[] pre : prerequisites) {
-            adj.get(pre[1]).add(pre[0]); // edge: pre[1] → pre[0]
+        for(int ed[] : pre){
+            int s = ed[0];
+            int d = ed[1];
+
+            adj.get(d).add(s);
         }
+        boolean vis[] = new boolean[numC];
 
-        int[] vis = new int[numCourses];
-        int[] pathVis = new int[numCourses];
+        boolean stack[] = new boolean[numC];
 
-        // Check each course
-        for (int i = 0; i < numCourses; i++) {
-            if (vis[i] == 0) {
-                if (dfs(i, adj, vis, pathVis)) return false; // cycle = cannot finish
+        for(int i = 0; i<numC; i++){
+            if(!vis[i]){
+                if(dfs(i,adj,vis, stack)) return false;
             }
         }
-
-        return true; // no cycle → can finish all courses
+        return true;
     }
 }
