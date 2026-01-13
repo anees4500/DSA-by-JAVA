4359991804b17769430486669f0774669eb4f024
anees4500 <mohammadanees4500@gmail.com>
2026-01-13 23:18:27 +0530

    Time: 3 ms (33.60%) | Memory: 48.2 MB (20.88%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,53 +1,38 @@
 class Solution {
-    public void helper(List<List<Integer>> adj, List<List<Integer>> ans,List<Integer> list , int src , int des, boolean vis[]){
 
-        list.add(src);
-        if(src==des){
-            ans.add(new ArrayList<>(list));
-            
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public void dfs(List<List<Integer>> adj, int curr, List<Integer> path) {
+
+        path.add(curr);
+
+        if (curr == adj.size() - 1) {
+            ans.add(new ArrayList<>(path));
+            path.remove(path.size() - 1); // BACKTRACK
             return;
         }
 
-        
-
-        for(int i : adj.get(src)){
-
-            if(!vis[i]){
-                vis[i] = true;
-                helper(adj,ans,list, i, des,vis);
-                vis[i] = false;
-                list.remove(list.size()-1);
-
-            }
-
+        for (int next : adj.get(curr)) {
+            dfs(adj, next, path);
         }
-         
 
+        path.remove(path.size() - 1); // BACKTRACK
     }
+
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+
         List<List<Integer>> adj = new ArrayList<>();
-
-        boolean vis[] = new boolean[graph.length];
-
-        for(int i = 0; i<graph.length; i++){
+        for (int i = 0; i < graph.length; i++) {
             adj.add(new ArrayList<>());
         }
 
-        for(int i = 0; i<graph.length ; i++){
-            int ed[] = graph[i];
-
-            for(int j = 0 ; j<ed.length; j++){
-                adj.get(i).add(ed[j]);
+        for (int i = 0; i < graph.length; i++) {
+            for (int v : graph[i]) {
+                adj.get(i).add(v);
             }
         }
 
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-
-        helper(adj,ans ,list, 0 , graph.length-1, vis);
-
+        dfs(adj, 0, new ArrayList<>());
         return ans;
-
-
     }
 }
