50cff9de70971c69695bc1d600968e5d335bd666
anees4500 <mohammadanees4500@gmail.com>
2026-01-21 02:13:55 +0530

    Time: 5 ms (28.42%) | Memory: 46.8 MB (20.58%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,92 +1,102 @@
+import java.util.*;
+
 class Solution {
 
-    public boolean isSafe(char[][] board, int row , int col, int n){
-        // horizontal
-        for(int j = 0; j<n; j++){
-            if(board[row][j]=='Q'){
+    public boolean dia(char[][] chess, int i, int j) {
+        if (i < 0 || j < 0 || i >= chess.length || j >= chess.length) {
+            return false;
+        }
+        return chess[i][j] == 'Q';
+    }
+
+    public boolean isSafe(char[][] chess, int i, int j) {
+
+        int k = i;
+        int l = j;
+
+        // ↖ left diagonal
+        while (k - 1 >= 0 && l - 1 >= 0) {
+            if (dia(chess, k - 1, l - 1)) {
                 return false;
             }
+            k--;
+            l--;
         }
-        // vertical
 
-        for(int i = 0; i<n; i++){
-            if(board[i][col]=='Q'){
+        k = i;
+        l = j;
+
+        // ⬆ same column
+        while (k - 1 >= 0) {
+            if (dia(chess, k - 1, l)) {
                 return false;
             }
+            k--;
         }
 
-        // left diagonal
+        k = i;
+        l = j;
 
-        for(int i = row, j = col; i>=0 && j>=0; i--,j--){
-            if(board[i][j]=='Q'){
+        // ↗ right diagonal
+        while (k - 1 >= 0 && l + 1 < chess.length) {
+            if (dia(chess, k - 1, l + 1)) {
                 return false;
             }
+            k--;
+            l++;
         }
-
-        for(int i = row, j=col; i>=0 && j<n; i--, j++){
-            if(board[i][j]=='Q'){
-                return false;
-            }
-        }
-
-        
 
         return true;
     }
 
+    public void print(char[][] chess, List<List<String>> answer) {
 
-    public void ans(char[][] board, int r, List<List<String>> ans){
+        List<String> list = new ArrayList<>();
 
-        if(r>board.length-1){
+        for (int i = 0; i < chess.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < chess.length; j++) {
+                sb.append(chess[i][j]);
+            }
+            list.add(sb.toString());
+        }
+
+        answer.add(new ArrayList<>(list));
+    }
+
+    public void ans(char[][] chess, int r, int n, List<List<String>> answer) {
+
+        if (r == n) {
             return;
         }
 
-        
-        for(int c = 0; c<board.length; c++){
-            if(isSafe(board,r,c,board.length)){
-                board[r][c] = 'Q';
-                if(r==board.length-1){
-                    print(board,ans);
+        for (int c = 0; c < n; c++) {
+            if (isSafe(chess, r, c)) {
+                chess[r][c] = 'Q';
+
+                if (r == n - 1) {
+                    print(chess, answer);
+                } else {
+                    ans(chess, r + 1, n, answer);
                 }
-                ans(board,r+1, ans);
+
+                chess[r][c] = '.';
             }
-            board[r][c]=  '.';
         }
     }
-
-
-    public void print(char[][] board, List<List<String>> List){
-        List<String> l = new ArrayList<>();
-        for(int i = 0; i<board.length; i++){
-            StringBuilder sb = new StringBuilder();
-            for(int j = 0; j<board.length; j++){
-               sb.append(board[i][j]);
-            }
-
-            l.add(sb.toString());
-
-        }
-
-        List.add(new ArrayList<>(l));
-    }
-
-
 
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
 
-        char[][] board = new char[n][n];
+        List<List<String>> answer = new ArrayList<>();
+        char[][] chess = new char[n][n];
 
-        for(int i = 0; i<board.length; i++){
-            Arrays.fill(board[i],'.');
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chess[i][j] = '.';
+            }
         }
 
-        // for(int i = 0; i<n; i++){
-        //     ans(board,0,ans);
-        // }
-
-        ans(board,0,ans);
-
-        return ans;
+        ans(chess, 0, n, answer);
+        return answer;
     }
 }
