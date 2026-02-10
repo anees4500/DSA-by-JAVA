cc1de8a5594ed7ee37118f1312fa5e1ae0e69a8c
anees4500 <mohammadanees4500@gmail.com>
2026-02-10 13:31:34 +0530

    Time: 2 ms (5.98%) | Memory: 44.4 MB (75.34%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -14,42 +14,27 @@
  * }
  */
 class Solution {
-    public int height(TreeNode root){
-
-        if(root==null){
-            return 0;
-        }
-
-        int lh = height(root.left);
-        int rh = height(root.right);
-        return 1+ Math.max(lh,rh);
-    }
-
-    public void Nth(TreeNode root, int n, List<Integer> list, int i){
-
-        if(root==null){
-            return ;
-        }
-
-        if(i==n){
-            list.add(root.val);
-            return;
-        }
-        i++;
-        Nth(root.left,n,list,i);
-        Nth(root.right,n,list,i);
-
-    }
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        
-        int ht = height(root);
-
         List<List<Integer>> ans = new ArrayList<>();
+        if(root==null) return ans;
 
-        for(int i = ht; i>0; i--){
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int level = queue.size();
+
             List<Integer> list = new ArrayList<>();
-            Nth(root,i,list,1);
-            ans.add(new ArrayList<>(list));
+
+            for(int i = 0; i<level; i++){
+                TreeNode node = queue.poll();
+
+                list.add(node.val);
+                if(node.left!=null)queue.offer(node.left);
+                if(node.right!=null)queue.offer(node.right);
+            }
+            ans.add(0,new ArrayList<>(list));
         }
 
         return ans;
