1a6bec6ee88d3b358aa424774da75c77b687290b
anees4500 <mohammadanees4500@gmail.com>
2026-02-10 23:59:12 +0530

    Time: 18 ms (43.46%) | Memory: 51.2 MB (6.05%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,60 @@
+ class Solution {
+
+    public int largest(int[] heights) {
+
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+
+                int height = heights[stack.pop()];
+                int pse = stack.isEmpty() ? -1 : stack.peek();
+                int width = i - pse - 1;
+
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+
+            int idx = stack.pop();
+            int height = heights[idx];
+            int pse = stack.isEmpty() ? -1 : stack.peek();
+            int width = n - pse - 1;
+
+            maxArea = Math.max(maxArea, height * width);
+        }
+
+        return maxArea;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+
+        if (matrix.length == 0) return 0;
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[] heights = new int[cols];
+        int maxAns = 0;
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+
+                if (matrix[i][j] == '1')
+                    heights[j]++;
+                else
+                    heights[j] = 0;
+            }
+
+            maxAns = Math.max(maxAns, largest(heights));
+        }
+
+        return maxAns;
+    }
+}
