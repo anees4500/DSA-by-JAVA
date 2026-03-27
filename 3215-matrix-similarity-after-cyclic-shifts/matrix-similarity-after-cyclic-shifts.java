4d538167b26bad9d8ba990fc18833071e0179f7e
anees4500 <mohammadanees4500@gmail.com>
2026-03-28 02:17:24 +0530

    Time: 1 ms (99.47%) | Memory: 46.9 MB (63.20%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -1,60 +1,28 @@
 class Solution {
 
-
-    public void leftRot(int[][] mat , int row){
-
-         int first = mat[row][0];
-        for(int i = 0; i<mat[0].length-1; i++){
-            mat[row][i] = mat[row][i+1];
-        }
-        mat[row][mat[0].length-1] = first;
-       
-    }
-
-    public void rightRot(int[][] mat, int row){
-
-        int first = mat[row][mat[0].length-1];
-        for(int i = mat[0].length-1; i>0; i--){
-            mat[row][i] = mat[row][i-1];
-        }
-
-        mat[row][0] = first;
-       
-    }
-
     public boolean areSimilar(int[][] mat, int k) {
 
-        int mask[][] = new int[mat.length][mat[0].length];
+        int n = mat[0].length;
+        k = k % n;
 
-        for(int i = 0; i<mat.length; i++){
-            for(int j = 0; j<mat[0].length; j++){
-                mask[i][j] = mat[i][j];
-            }
-        }
+        for(int i = 0; i < mat.length; i++){
 
-        
-        for(int i = 0; i<k; i++){
+            for(int j = 0; j < n; j++){
 
-            for(int j = 0; j<mat.length; j++){
-                if(j%2==0){
-                    leftRot(mat,j);
+                int newCol;
+
+                if(i % 2 == 0){
+                    newCol = (j + k) % n; // left shift
+                } else {
+                    newCol = (j - k + n) % n; // right shift
                 }
-                else{
-                    rightRot(mat,j);
-                }
-            }
-        }
 
-        for(int i = 0; i<mat.length; i++){
-            for(int j = 0; j<mat[0].length; j++){
-                if(mask[i][j] != mat[i][j]){
+                if(mat[i][j] != mat[i][newCol]){
                     return false;
                 }
             }
         }
 
         return true;
-
-
     }
 }
