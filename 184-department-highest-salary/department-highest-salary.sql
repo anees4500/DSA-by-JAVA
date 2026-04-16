e844a3baeaab2e5a7baf366ed14f802640a2d900
anees4500 <mohammadanees4500@gmail.com>
2026-04-17 00:42:52 +0530

    Time: 819 ms (35.74%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,28 +1,28 @@
 # Write your MySQL query statement below
 
 
--- SELECT 
---     D.name AS Department,
---     E.name AS Employee,
---     E.salary AS Salary
--- FROM Employee E
--- JOIN Department D 
---     ON D.id = E.departmentId
--- WHERE (E.departmentId, E.salary) IN (
---     SELECT departmentId, MAX(salary)
---     FROM Employee
---     GROUP BY departmentId
--- );
-
 SELECT 
     D.name AS Department,
     E.name AS Employee,
     E.salary AS Salary
-FROM (
-    SELECT *,
-           RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS rnk
-    FROM Employee
-) E
+FROM Employee E
 JOIN Department D 
-ON E.departmentId = D.id
-WHERE rnk = 1;
+    ON D.id = E.departmentId
+WHERE (E.departmentId, E.salary) IN (
+    SELECT departmentId, MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
+);
+
+-- SELECT 
+--     D.name AS Department,
+--     E.name AS Employee,
+--     E.salary AS Salary
+-- FROM (
+--     SELECT *,
+--            RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS rnk
+--     FROM Employee
+-- ) E
+-- JOIN Department D 
+-- ON E.departmentId = D.id
+-- WHERE rnk = 1;
