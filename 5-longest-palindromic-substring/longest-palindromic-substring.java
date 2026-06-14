2618805cd0cf71f5cb56eef716fa24ab6b412b4d
anees4500 <mohammadanees4500@gmail.com>
2026-06-15 02:02:22 +0530

    Time: 165 ms (21.50%) | Memory: 102 MB (5.15%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,19 +1,39 @@
 class Solution {
-
     public String longestPalindrome(String s) {
-
         int n = s.length();
+
         int dp[][] = new int[n][n];
 
-        for(int i = 0 ; i<n; i++){
+        if(s.length()==1){
+            return s;
+        }
+
+        if(s.length()==2){
+            if(s.charAt(0)==s.charAt(1)){
+                return s;
+            }
+            else{
+                return s.substring(0,1);
+            }
+        }
+
+        for(int i = 0; i<n; i++){
             dp[i][i] = 1;
         }
 
-        for(int i  = 0; i<n-1; i++){
+        for(int i = 0; i<n-1; i++){
             if(s.charAt(i)==s.charAt(i+1)){
                 dp[i][i+1] = 1;
             }
         }
+
+        // for(int i = 0; i<n-1; i++){
+        //     for(int j = i+2; j<n; j++){
+        //         if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==1){
+        //             dp[i][j] = 1;
+        //         }
+        //     }
+        // }
 
         for(int k = 2; k<n; k++){
             for(int i = 0; i<n-k; i++){
@@ -24,26 +44,23 @@ class Solution {
             }
         }
 
-        
-
         StringBuilder sb = new StringBuilder();
 
 
         for(int i = 0; i<n; i++){
             for(int j = i; j<n; j++){
-                if( dp[i][j] == 1 && sb.length() < (j-i+1)){
-                    sb = new StringBuilder();
-                    
+                if(dp[i][j] == 1){
 
-                    sb.append(s.substring(i,j+1));
+                    if(sb.length()<(j - i +1)){
+                        sb = new StringBuilder();
+                        sb.append(s.substring(i,j+1));
+                    }
+
                 }
             }
         }
 
         return sb.toString();
-
-
-
 
     }
 }
