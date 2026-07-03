d58f0c8de407bc4c7b77bcd68e19a539a632c3dc
anees4500 <mohammadanees4500@gmail.com>
2026-07-03 15:13:59 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (59.20%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,23 +1,50 @@
- class Solution {
-    public int Height(TreeNode root){
-        if(root == null){
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    boolean ans = true;
+
+    public int isBalance(TreeNode root) {
+
+        if(root==null){
             return 0;
         }
-        int lh = Height(root.left);
-        int rh = Height(root.right);
-        return 1 + Math.max(lh, rh);
+
+        int left = isBalance(root.left);
+        int right = isBalance(root.right);
+
+        int h = Math.max(left , right) +  1;
+
+        if(Math.abs(left - right)>1){
+            ans = false;
+        }
+
+        return h;
+    
+        
     }
 
-    public boolean isBalanced(TreeNode root) {
-        if(root == null){   // ✅ handle empty tree
-            return true;
-        }
-        int ls = Height(root.left);
-        int rs = Height(root.right);
 
-        if(Math.abs(ls - rs) > 1){
-            return false;
-        }
-        return isBalanced(root.left) && isBalanced(root.right);
+    public boolean isBalanced(TreeNode root) {
+
+        isBalance(root);
+
+        return ans;
+        
+    
+        
     }
 }
