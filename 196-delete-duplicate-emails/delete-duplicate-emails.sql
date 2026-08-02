df5021ea04476f5f6c7caaf66cb048f9f05bf4d1
anees4500 <mohammadanees4500@gmail.com>
2026-08-02 23:55:40 +0530

    Time: 730 ms (86.01%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,22 @@
 # Write your MySQL query statement below
-delete p1
-from Person p1 , Person p2
-where p1.email = p2.email
-and p1.id>p2.id;
+-- delete p1
+-- from Person p1 , Person p2
+-- where p1.email = p2.email
+-- and p1.id>p2.id;
+
+-- delete from Person
+-- where id  not in (
+--     select MIN(id)
+--     from Person 
+--     group by email
+-- );
+
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT id
+    FROM (
+        SELECT MIN(id) AS id
+        FROM Person
+        GROUP BY email
+    ) AS temp
+);
