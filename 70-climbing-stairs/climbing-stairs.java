9f5dcb593d254f24f0992891ec7879776fd9f01c
anees4500 <mohammadanees4500@gmail.com>
2026-08-17 18:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (55.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -5,7 +5,6 @@ class Solution {
         if(n==0){
             return 1;
         }
-
         if(n<0){
             return 0;
         }
@@ -13,21 +12,18 @@ class Solution {
         if(dp[n]!=-1){
             return dp[n];
         }
-        
-        
-        return dp[n] = helper(n-1,dp) + helper(n-2,dp);
+
+
+        return dp[n] = helper(n-1, dp) + helper(n-2, dp);
     }
-
-
     public int climbStairs(int n) {
-        
 
         int dp[] = new int[n+1];
 
         Arrays.fill(dp , -1);
 
-
         return helper(n,dp);
-
+        
+        
     }
 }
