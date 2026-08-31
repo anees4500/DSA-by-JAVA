910e33cdf90df349461ae51805e9efa2f50fb69d
anees4500 <mohammadanees4500@gmail.com>
2026-08-31 15:01:36 +0530

    Time: 2 ms (100.00%) | Memory: 86.8 MB (39.45%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -1,33 +1,50 @@
- class Solution {
+class Solution {
     public int minimumDeletions(int[] nums) {
-        int n = nums.length;
-        int minVal = Integer.MAX_VALUE;
-        int maxVal = Integer.MIN_VALUE;
-        int minIdx = 0, maxIdx = 0;
+        int mIdx = -1;
+        int minIdx = -1;
 
-        // find indices of min and max
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < minVal) {
-                minVal = nums[i];
-                minIdx = i;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0 ; i<nums.length; i++){
+            if(max<nums[i]){
+                max = nums[i];
+                mIdx = i;
             }
-            if (nums[i] > maxVal) {
-                maxVal = nums[i];
-                maxIdx = i;
+
+            if(min>nums[i]){
+                min = nums[i];
+                minIdx = i;
             }
         }
 
-        // Option 1: remove from front both elements
-        int front = Math.max(minIdx, maxIdx) + 1;
+        int both = 0;
 
-        // Option 2: remove from back both elements
-        int back = n - Math.min(minIdx, maxIdx);
+        if(mIdx<minIdx){
+            both = mIdx+1 +  nums.length - minIdx;
+        }
+        else{
+            both = minIdx+1  + nums.length - mIdx ;
+        }
 
-        // Option 3: remove one from front, one from back
-        int both = Math.min(minIdx + 1 + (n - maxIdx),
-                            maxIdx + 1 + (n - minIdx));
+        int left = 0;
 
-        // Minimum of all 3
-        return Math.min(front, Math.min(back, both));
+        if(mIdx>minIdx){
+            left = mIdx + 1;
+        }
+        else{
+            left = minIdx+1;
+        }
+
+        int right = 0;
+
+        if(mIdx<minIdx){
+            right = nums.length - mIdx;
+        }
+        else{
+            right = nums.length - minIdx;
+        }
+
+        return Math.min(left, Math.min(right, both));
     }
 }
